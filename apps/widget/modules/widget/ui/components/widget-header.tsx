af4613f5f107ecf0e5192700ff670813a87b3942
druswer <doniyorforjob@gmail.com>
2026-03-08 13:37:38 +0500

    style: remove vertical padding from widget header

--- apps/widget/modules/widget/ui/components/widget-header.tsx
+++ apps/widget/modules/widget/ui/components/widget-header.tsx
@@ -9,7 +9,7 @@ export const WidgetHeader = ({
 }) => {
   return (
     <header className={cn(
-      "bg-gradient-to-b from-primary to-[#0b63f3] p-4 text-primary-foreground",
+      "bg-gradient-to-b from-primary to-[#0b63f3] px-4 py-2 text-primary-foreground",
       className,
     )}>
       {children}
--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -73,7 +73,7 @@ export const WidgetAuthScreen = () => {
   return (
     <>
       <WidgetHeader>
-        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+        <div className="flex flex-col justify-between gap-y-2 px-2 font-semibold">
           <p className="text-3xl">
             Namangan davlat texnika universiteti
           </p>
